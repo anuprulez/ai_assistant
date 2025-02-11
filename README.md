ddc5aab8a9095530adc900bd17525b5de270614b
Anup Kumar <anup.rulez@gmail.com>
2025-02-11 18:35:03 +0000

    add docker command

--- README.md
+++ README.md
@@ -6,3 +6,14 @@
 - export CXX=g++
 - CMAKE_ARGS="-DLLAMA_OPENMP=ON -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++" pip install llama-cpp-python --no-cache-dir
 - or CMAKE_ARGS="-DLLAMA_OPENMP=ON -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++" pip install llama-cpp-python --no-cache-dir --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu121
+
+### Run llama.cpp via Docker
+
+https://github.com/unslothai/llama.cpp/blob/master/docs/docker.md
+
+on CPUs
+
+`docker run -v /home/ubuntu/data/ai_assistant/ai_assistant/models:/models ghcr.io/ggerganov/llama.cpp:light -m /models/DeepSeek-R1-Distill-Qwen-32B-Q4_K_M.gguf -p "Building a website can be done
+ in 10 simple steps:" -n 512`
+
+ on GPUs
--- run_llama_cpp.sh
+++ run_llama_cpp.sh
@@ -1,7 +1,11 @@
 ./llama.cpp/build/bin/llama-cli \
-       --model models/DeepSeek-R1-Distill-Qwen-32B-Q8_0.gguf \
+       --model models/DeepSeek-R1-Distill-Qwen-32B-Q4_K_M.gguf \
        --cache-type-k q8_0 \
        --threads 16 \
        --prompt '<｜User｜>What is 1+1?<｜Assistant｜>' \
-       --n-gpu-layers 50
+       --threads 16 \
+       ##--n-gpu-layers 40
        -no-cnv
+
+# I have a dataset in which we are looking at canonical events vs noncanonical events (intro retention, alternative splicing, etc.). We were considering using STAR in a way were we can obtain counts only for canonical events and noncanonical events, to compare the quantifications afterwards. Do you think it is possible to use solely STAR to get these quantifications? If so, which would be the arguments one should change?
+# https://github.com/alexdobin/STAR/issues/2259
